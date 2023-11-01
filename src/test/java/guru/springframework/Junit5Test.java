b8a96e4694b1d6923d45ff53177b69ff974c3371
Isaak <krutisaak099@gmail.com>
2023-11-01 11:28:26 -0400

    Added JUnit 4 and 5 Tests

---
+++ src/test/java/guru/springframework/Junit5Test.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5Test {
+
+    @Test
+    void testJunit5() {
+        System.out.println("Junit 5 Running!!!!");
+    }
+}
